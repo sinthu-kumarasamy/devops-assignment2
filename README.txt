c00c9267d566d644a432c62e48814c6a50db9782
Sinthu <kumarasamy.s@husky.neu.edu>
2021-09-27 09:21:23 -0400

    Adding assignment2

---
+++ README.txt
@@ -0,0 +1,15 @@
+Team members:-
+Sinthu Kumarasamy
+Shivam Thakkar
+
+Assignment 2
+
+mvcrs is the folder containing the MVC web app.
+rsapi is the folder containing the WEBAPI project.
+
+In the rsapi,
+we have created a SongController which is returning the Song lyrics based on the request.
+In the Song.cs we have 2 ways to return the lyrics, One is to store the lyrical text in variable and return it and other is directly return the string from read file.
+
+And in the mvcrs, 
+we are reading the response from the server and setting up with the response in between div with the given IDs.
---
+++ rsapi/Song.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+namespace rsapi
+{
+    public class Song
+    {
+        public string Miracle { get; set; }
+        public string Song2 { get; set; }
+
+        public Song()
+        {
+            if (File.Exists("song.txt")) {
+                Miracle = File.ReadAllText("song.txt");
+            } else {
+                Miracle = "??";
+            }
+        }
+
+        public String SongBM(){
+            if (File.Exists("song2.txt")) {
+                return File.ReadAllText("song2.txt");
+            } else {
+                return "??";
+            }
+        }
+    }
+}
